672bcc11f8543ac3ada23e32b2c3055a670b99d8
Darren Tjakradinata <darren0208.dc@gmail.com>
2024-04-03 21:01:54 +0700

    chore(@/landing/components/layout): responsive footer

--- apps/landing/components/layout/footer.tsx
+++ apps/landing/components/layout/footer.tsx
@@ -6,8 +6,8 @@ export function Footer(): JSX.Element {
   return (
     <>
       <div className="w-screen bg-background">
-        <SectionContainer className="flex flex-row justify-between gap-8">
-          <div className="flex flex-col gap-8">
+        <SectionContainer className="flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row items-center sm:items-center md:items-start lg:items-start xl:items-start 2xl:items-start justify-between gap-8">
+          <div className="flex flex-col gap-8 items-center sm:items-center md:items-start lg:items-start xl:items-start 2xl:items-start">
             <Image
               alt="Brand Logo"
               height={60}
@@ -15,24 +15,24 @@ export function Footer(): JSX.Element {
               width={345}
             />
 
-            <p className="text-secondary text-xl max-w-[485px]">
+            <p className="text-secondary text-xl max-w-[485px] text-center sm:text-center md:text-left lg:text-left xl:text-left 2xl:text-left">
               Pengelolaan klinik menjadi lebih mudah dan lancar. SatuKlinik
               hadir untuk menyederhanakan setiap aspek operasional klinik Anda.
             </p>
           </div>
 
-          <div className="flex flex-col gap-4 text-secondary text-xl max-w-[324px]">
+          <div className="flex flex-col gap-4 text-secondary text-xl max-w-[324px] items-center sm:items-center md:items-start lg:items-start xl:items-start 2xl:items-start">
             <p className="text-base font-extrabold">TENTANG</p>
             <p>Tentang Kami</p>
             <p>Syarat & Ketentuan</p>
             <p>Kebijakan Privasi</p>
           </div>
 
-          <div className="flex flex-col gap-4 text-secondary text-xl max-w-[324px]">
+          <div className="flex flex-col gap-4 text-secondary text-xl max-w-[324px] items-center sm:items-center md:items-start lg:items-start xl:items-start 2xl:items-start">
             <p className="text-base font-extrabold">KONTAK</p>
             <p>0812-3456-7890</p>
             <p>solusi@satuklinik.com</p>
-            <p>
+            <p className="text-center sm:text-center md:text-left lg:text-left xl:text-left 2xl:text-left">
               Jl. Elang IV, Sawah Lama, Kec. Ciputat, Kota Tangerang Selatan,
               Banten 15413
             </p>
@@ -41,7 +41,7 @@ export function Footer(): JSX.Element {
       </div>
       <div className="w-screen bg-secondary items-center justify-center py-3">
         <p className="text-xl text-background text-center">
-          Copyright ©2024 satuklinik.com
+          Copyright ©{new Date().getFullYear()} satuklinik.com
         </p>
       </div>
     </>
