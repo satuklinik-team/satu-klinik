b2bb9d5dd8c971a0991e3576fddff21f266d6a13
Darren Tjakradinata <darren0208.dc@gmail.com>
2024-04-03 17:08:08 +0700

    chore(@/landing/features/home): responsive hero

--- apps/landing/features/home/components/hero.tsx
+++ apps/landing/features/home/components/hero.tsx
@@ -6,10 +6,10 @@ export function Hero(): JSX.Element {
   return (
     <div className="w-screen">
       <SectionContainer className="flex flex-col items-center">
-        <h1 className="text-8xl text-center font-extrabold text-secondary">
+        <h1 className="text-6xl sm:text-6xl md:text-7xl lg:text-7xl xl:text-8xl 2xl:text-8xl text-center font-extrabold text-secondary">
           Klinik Resmi Satu Sehat.
         </h1>
-        <h1 className="text-8xl text-center font-extrabold text-secondary">
+        <h1 className="text-6xl sm:text-6xl md:text-7xl lg:text-7xl xl:text-8xl 2xl:text-8xl text-center font-extrabold text-secondary">
           Tanpa Ribet.
         </h1>
         <p className="text-center text-secondary mt-[30px] mb-[34px]">
@@ -22,10 +22,7 @@ export function Hero(): JSX.Element {
         <img
           alt="Hero"
           src="/home/hero-image.png"
-          style={{
-            width: "100%",
-            maxWidth: 1015,
-          }}
+          className="w-full max-w-[1015px]"
         />
       </SectionContainer>
     </div>
