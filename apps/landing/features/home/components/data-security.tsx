047d9740eae9a2e9807e7db5b692474c7fcad286
Darren Tjakradinata <darren0208.dc@gmail.com>
2024-04-03 20:37:55 +0700

    chore(@/landing/features/home): responsive data security

--- apps/landing/features/home/components/data-security.tsx
+++ apps/landing/features/home/components/data-security.tsx
@@ -9,15 +9,15 @@ import { FeatureBenefitItem } from "./ui/feature-benefit-item";
 
 export function DataSecurity(): JSX.Element {
   return (
-    <div className="relative w-screen bg-primary">
-      <SectionContainer>
+    <div className="w-screen bg-primary">
+      <SectionContainer className="relative">
         <Badge className="text-primary bg-secondary font-extrabold px-3 py-2 rounded-none hover:bg-secondary">
           KEAMANAN DATA
         </Badge>
         <h2 className="text-secondary text-4xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-[60px] 2xl:text-[60px] font-extrabold mt-5 mb-5 sm:mb-5 md:mb-5 lg:mb-12 xl:mb-12 2xl:mb-12">
           Perlindungan Data yang Komprehensif
         </h2>
-        <div className="flex flex-col sm:flex-col md:flex-col lg:flex-col xl:flex-row 2xl:flex-row justify-between gap-8 sm:gap-12 md:gap-16 lg:gap-16 xl:gap-24 2xl:gap-24">
+        <div className="flex flex-col sm:flex-col md:flex-col lg:flex-col xl:flex-row 2xl:flex-row justify-between items-start sm:items-start md:items-start xl:items-center 2xl:items-center gap-8 sm:gap-12 md:gap-16 lg:gap-16 xl:gap-24 2xl:gap-24">
           <div className="flex flex-col gap-10">
             <div className="flex flex-col gap-5">
               <div className="w-[82px] h-3 bg-primary rounded-md" />
@@ -45,23 +45,24 @@ export function DataSecurity(): JSX.Element {
               />
             ))}
           </div>
-          <Image
-            alt="Data Security Image"
-            className="shrink-0"
-            height={542}
-            src="/home/data-security-image.png"
-            width={597}
-          />
+          <div className=" w-fit h-fit">
+            <Image
+              alt="Data Security Image"
+              className="shrink-0"
+              height={542}
+              src="/home/data-security-image.png"
+              width={597}
+            />
+          </div>
         </div>
+        <Image
+          alt="Brand Logo"
+          className="absolute right-0 bottom-0 opacity-40"
+          height={263.45}
+          src="/brand-logo-2.png"
+          width={265.13}
+        />
       </SectionContainer>
-
-      <Image
-        alt="Brand Logo"
-        className="absolute right-0 bottom-0 opacity-40"
-        height={263.45}
-        src="/brand-logo-2.png"
-        width={265.13}
-      />
     </div>
   );
 }
