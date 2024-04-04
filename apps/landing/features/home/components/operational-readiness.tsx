bcda58df262dd3c06ed214dab9582af14088c10f
Darren Tjakradinata <darren0208.dc@gmail.com>
2024-04-04 15:31:37 +0700

    chore(@/landing/features/home): use shorthand

--- apps/landing/features/home/components/operational-readiness.tsx
+++ apps/landing/features/home/components/operational-readiness.tsx
@@ -11,7 +11,7 @@ export function OperationalReadiness(): JSX.Element {
         <Badge className="text-background bg-secondary font-extrabold px-3 py-2 rounded-none hover:bg-secondary">
           KELENGKAPAN OPERASIONAL
         </Badge>
-        <h2 className="text-secondary text-center text-4xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-[60px] 2xl:text-[60px] font-extrabold mt-5 mb-[32px] sm:mb-[48px] md:mb-[64px] lg:mb-[64px] xl:mb-[79px] 2xl:mb-[79px]">
+        <h2 className="text-secondary text-center text-4xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-[60px] 2xl:text-[60px] font-extrabold mt-5 mb-[32px] sm:mb-xl md:mb-[64px] lg:mb-[64px] xl:mb-[79px] 2xl:mb-[79px]">
           Kami Siapkan Kebutuhan Operasional
         </h2>
         <Image
