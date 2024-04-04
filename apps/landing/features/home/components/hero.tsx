3f88063369b25575bcff3654b6e5e09dd85c33da
Darren Tjakradinata <darren0208.dc@gmail.com>
2024-04-04 08:21:26 +0700

    chore(@/landing/features/home): better image

--- apps/landing/features/home/components/hero.tsx
+++ apps/landing/features/home/components/hero.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import { Button } from "@/components/ui/button";
 
 import { SectionContainer } from "./shared/section-container";
@@ -19,10 +21,12 @@ export function Hero(): JSX.Element {
         <Button className="px-6 py-5 text-black text-2xl font-bold mb-[44px] shadow-[0_-2px_2px_0px_rgba(0, 0, 0, 0.25)_inset]">
           Jadwalkan Demo
         </Button>
-        <img
+        <Image
           alt="Hero"
-          src="/home/hero-image.png"
           className="w-full max-w-[1015px]"
+          height={857}
+          src="/home/hero-image.png"
+          width={1015}
         />
       </SectionContainer>
     </div>
