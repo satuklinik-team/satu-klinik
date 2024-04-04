5179c82f547e3aec7f18f3acaeb3209d1ecac798
Darren Tjakradinata <darren0208.dc@gmail.com>
2024-04-04 10:01:54 +0700

    chore(@/landing/features/home): on progress responsive digitalization

--- apps/landing/features/home/components/digitalization.tsx
+++ apps/landing/features/home/components/digitalization.tsx
@@ -1,4 +1,4 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 import { CheckBadgeIcon } from "@/components/icons/check-badge";
 import { Badge } from "@/components/ui/badge";
@@ -16,49 +16,30 @@ export function Digitalization(): JSX.Element {
         <h2 className="text-secondary text-5xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-[60px] 2xl:text-[60px] font-extrabold mb-12 mt-5">
           Pelayanan menjadi lebih mudah
         </h2>
-        <div className="flex flex-col gap-10">
-          {features.map((item) => (
-            <div className="flex flex-row gap-4" key={item.title}>
-              <CheckBadgeIcon className="fill-secondary" size={40} />
-              <div className="flex flex-col gap-2">
-                <p className="text-secondary text-[32px] font-bold">
-                  {item.title}
-                </p>
-                <p className="text-secondary text-xl max-w-[598px]">
-                  {item.description}
-                </p>
+        <div className="mb-[280px] sm:mb-[640px] md:mb-[640px] lg:mb-[640px] xl:mb-0 2xl:mb-0">
+          <div className="flex flex-col gap-10">
+            {features.map((item) => (
+              <div className="flex flex-row gap-4" key={item.title}>
+                <CheckBadgeIcon className="fill-secondary" size={40} />
+                <div className="flex flex-col gap-2">
+                  <p className="text-secondary text-[32px] font-bold">
+                    {item.title}
+                  </p>
+                  <p className="text-secondary text-xl max-w-[598px]">
+                    {item.description}
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          <Image
+            alt="Digitalization Image Group"
+            className="absolute bottom-0 translate-x-1/2 sm:translate-x-1/2 md:translate-x-1/2 lg:translate-x-1/2 right-1/2 sm:right-1/2 md:right-1/2 lg:right-1/2 xl:right-[40px] 2xl:right-[0px] xl:w-[502px] 2xl:w-[600.35px] xl:h-[480.63px] 2xl:h-[600px]"
+            height={642}
+            src="/home/digitalization-image-group.png"
+            width={652.35}
+          />
         </div>
-        {/* <div className="absolute bottom-0 right-[100px]">
-          <Image
-            alt="Digitalization Preview"
-            className="translate-y-[86px]"
-            height={229.59}
-            src="/home/digitalization-preview.png"
-            width={470}
-          />
-          <Image
-            alt="Curly Arrow"
-            className="translate-x-[240px] translate-y-[114px]"
-            height={130.8}
-            src="/curly-arrow.png"
-            width={46.58}
-          />
-          <Image
-            alt="Digitalization Image"
-            height={269.78}
-            src="/home/digitalization-image.png"
-            width={457.35}
-          />
-          <Image
-            alt="Brand Logo"
-            height={192.44}
-            src="/brand-logo-2.png"
-            width={191.22}
-          />
-        </div> */}
       </SectionContainer>
     </div>
   );
