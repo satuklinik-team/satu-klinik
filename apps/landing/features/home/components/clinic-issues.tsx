d02a101e256d70afc583ee19c7a0352093ec3a9f
Darren Tjakradinata <darren0208.dc@gmail.com>
2024-04-05 08:19:43 +0700

    chore(@/landing/features/home): apply new utility class extension

--- apps/landing/features/home/components/clinic-issues.tsx
+++ apps/landing/features/home/components/clinic-issues.tsx
@@ -5,14 +5,14 @@ export function ClinicIssues(): JSX.Element {
   return (
     <div className="w-screen bg-foreground">
       <SectionContainer>
-        <h2 className="text-background text-5xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-[64px] 2xl:text-[64px] font-extrabold mb-2xl">
+        <h2 className="text-background text-5xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-type-display 2xl:text-type-display font-extrabold mb-2xl">
           Masalah klinik saat ini
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 gap-10">
           {clinicIssues.map((item) => (
             <div key={item.title}>
               <div className="w-10 h-3 bg-primary rounded-md" />
-              <p className="font-bold text-[32px] text-background mb-sm mt-md">
+              <p className="font-bold text-type-heading4 text-background mb-sm mt-md">
                 {item.title}
               </p>
               <p className="text-xl	text-background">{item.description}</p>
