40b9908db4cf8813101960047e5157cdb9803be7
Darren Tjakradinata <darren0208.dc@gmail.com>
2024-04-18 16:28:22 +0700

    feat(@/apps/web/features/clinic-activity): add clinic activity page

---
+++ apps/web/features/clinic-activity/routes/clinic-activity-page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export function ClinicActivityPage(): JSX.Element {
+  return (
+    <div className="h-full">
+      <div className="mb-6 flex flex-col gap-2">
+        <h1 className="text-4xl font-semibold">Activity</h1>
+        <p className="text-muted-foreground">Manage all activities</p>
+      </div>
+    </div>
+  );
+}
