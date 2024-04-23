b09607cc3a599be972eb020cbb529cc766b2a08e
Muflih Naufal Maxi <muflihmaxi@gmail.com>
2024-04-23 15:23:32 +0700

    fix(apps/core-backend/patient): remove public decorator from patient endpoints

--- apps/core-backend/src/patients/patients.controller.ts
+++ apps/core-backend/src/patients/patients.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { PatientsService } from './patients.service';
 import { CreatePatientDto } from './dto/create-patient.dto';
-import { Public, QueryPayload } from 'src/utils';
+import { QueryPayload } from 'src/utils';
 import { CreateVitalSignDto } from '../patients-vital-sign/dto/create-vital-sign.dto';
 import { PatientsVitalSignService } from 'src/patients-vital-sign/patients-vital-sign.service';
 import { Prisma } from '@prisma/client';
@@ -13,20 +13,17 @@ export class PatientsController {
     private readonly patientVitalSignService: PatientsVitalSignService,
   ) {}
   @Post()
-  @Public()
   createPatient(@Body() createPatientDto: CreatePatientDto) {
     return this.patientService.create(createPatientDto);
   }
 
   @Post(':id/vital_sign')
-  @Public()
   createVitalSign(@Param('id') id: string, @Body() dto: CreateVitalSignDto) {
     dto.patientId = id;
     return this.patientVitalSignService.create(dto);
   }
 
   @Get()
-  @Public()
   async findAll(
     @QueryPayload() dto: Prisma.PatientFindManyArgs['select'],
     @Query('search') search: string,
