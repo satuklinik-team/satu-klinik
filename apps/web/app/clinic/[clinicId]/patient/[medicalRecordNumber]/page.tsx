93978b8d07ce9ee77853bf3c54851769b126f4d0
Darren Tjakradinata <darren0208.dc@gmail.com>
2024-04-25 17:16:29 +0700

    chore(@/apps/web/features): init edit and detail patient ui page

---
+++ apps/web/app/clinic/[clinicId]/patient/[medicalRecordNumber]/page.tsx
@@ -0,0 +1,5 @@
+import { ClinicPatientDetailPage } from "@/features/clinic-patient-detail/routes/clinic-patient-detail-page";
+
+export default function Page(): JSX.Element {
+  return <ClinicPatientDetailPage />;
+}
