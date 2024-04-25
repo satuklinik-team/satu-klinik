9c7b5544f5e6c5173bea64c09e167e9bf33acbef
Darren Tjakradinata <darren0208.dc@gmail.com>
2024-04-25 10:03:22 +0700

    chore(@/apps/web/features/clinic-patient): complete vitals

--- apps/web/components/ui/icon.tsx
+++ apps/web/components/ui/icon.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import * as React from "react";
-
-export type IconProps = React.PropsWithChildren & {
+export type IconProps = React.SVGProps<SVGSVGElement> & {
   size?: number;
   className?: string;
 };
@@ -11,6 +9,7 @@ export const Icon = ({
   children,
   className = "fill-black",
   size = 24,
+  fill,
   ...props
 }: IconProps) => {
   return (
@@ -18,6 +17,7 @@ export const Icon = ({
       xmlns="http://www.w3.org/2000/svg"
       height={size}
       width={size}
+      fill={fill}
       className={className}
       viewBox="0 0 24 24"
       {...props}
