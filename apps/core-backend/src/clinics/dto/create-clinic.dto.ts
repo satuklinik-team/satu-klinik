6077ff723c95da208ba83cd9d36615993cedc2ac
Muflih Naufal Maxi <muflihmaxi@gmail.com>
2024-04-25 17:30:59 +0700

    fix(apps/core-backend/clinics): remove unused imports

--- apps/core-backend/src/clinics/dto/create-clinic.dto.ts
+++ apps/core-backend/src/clinics/dto/create-clinic.dto.ts
@@ -1,13 +1,5 @@
-import { Type } from 'class-transformer';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsObject,
-  IsString,
-  ValidateNested,
-} from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { RegisterDto } from 'src/auth/dto';
-import { OrganizationAddress } from 'src/satusehat-organization/dto';
 
 export class CreateClinicDto extends RegisterDto {
   @IsString()
