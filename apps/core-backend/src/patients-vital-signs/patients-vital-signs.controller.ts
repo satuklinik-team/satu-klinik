2c105eed3ecbb97da327bb31505001eafc376300
Muflih Naufal Maxi <muflihmaxi@gmail.com>
2024-04-30 07:27:11 +0700

    refactor(apps/core-backend/patient-vital-signs): move vital sign endpoint

--- apps/core-backend/src/patients-vital-signs/patients-vital-signs.controller.ts
+++ apps/core-backend/src/patients-vital-signs/patients-vital-signs.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { CreateVitalSignDto } from './dto/create-vital-sign.dto';
+import { PatientsVitalSignsService } from './patients-vital-signs.service';
 
-@Controller('patients-vital-sign')
-export class PatientsVitalSignsController {}
+@Controller('patients-vital-signs')
+export class PatientsVitalSignsController {
+  constructor(
+    private readonly patientVitalSignService: PatientsVitalSignsService,
+  ) {}
+
+  @Post(':id')
+  createVitalSign(@Param('id') id: string, @Body() dto: CreateVitalSignDto) {
+    dto.patientId = id;
+    return this.patientVitalSignService.create(dto);
+  }
+}
--- apps/core-backend/src/patients/patients.controller.ts
+++ apps/core-backend/src/patients/patients.controller.ts
@@ -1,26 +1,16 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { PatientsService } from './patients.service';
 import { CreatePatientDto } from './dto/create-patient.dto';
 import { QueryPayload } from 'src/utils';
-import { CreateVitalSignDto } from '../patients-vital-signs/dto/create-vital-sign.dto';
-import { PatientsVitalSignsService } from 'src/patients-vital-signs/patients-vital-signs.service';
 import { Prisma } from '@prisma/client';
 
 @Controller('patients')
 export class PatientsController {
-  constructor(
-    private readonly patientService: PatientsService,
-    private readonly patientVitalSignService: PatientsVitalSignsService,
-  ) {}
+  constructor(private readonly patientService: PatientsService) {}
+
   @Post()
   createPatient(@Body() createPatientDto: CreatePatientDto) {
     return this.patientService.create(createPatientDto);
-  }
-
-  @Post(':id/vital_sign')
-  createVitalSign(@Param('id') id: string, @Body() dto: CreateVitalSignDto) {
-    dto.patientId = id;
-    return this.patientVitalSignService.create(dto);
   }
 
   @Get()
