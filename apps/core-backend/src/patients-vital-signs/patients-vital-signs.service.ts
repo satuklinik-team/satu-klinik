ed39e39de4468f768cf3dd2a1c440a56e5e1edcb
Muflih Naufal Maxi <muflihmaxi@gmail.com>
2024-05-01 13:14:11 +0700

    fix(apps/core-backend/patient-vital-signs): add status when creating vital sign

--- apps/core-backend/src/patients-vital-signs/patients-vital-signs.service.ts
+++ apps/core-backend/src/patients-vital-signs/patients-vital-signs.service.ts
@@ -18,6 +18,7 @@ export class PatientsVitalSignsService {
         patientId: dto.patientId,
         doctor: '',
         visitLabel: now.toLocaleDateString(),
+        status: 'e1',
         vitalSign: {
           create: {
             height: dto.height,
