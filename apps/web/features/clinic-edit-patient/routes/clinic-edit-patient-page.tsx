d37b4529f440d9dbef5ded9976f6ea6ca96b1054
Darren <darren.christian@dwp.co.id>
2024-06-01 18:37:53 +0700

    chore(@/apps/web/app/providers): add error handling

--- apps/web/features/clinic-edit-patient/routes/clinic-edit-patient-page.tsx
+++ apps/web/features/clinic-edit-patient/routes/clinic-edit-patient-page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { useParams, usePathname } from "next/navigation";
+import { useQueryClient } from "@tanstack/react-query";
+import dayjs from "dayjs";
+import { useParams, usePathname, useRouter } from "next/navigation";
+import { useCallback } from "react";
 
 import {
   Breadcrumb,
@@ -9,12 +12,48 @@ import {
   BreadcrumbList,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { useToast } from "@/components/ui/use-toast";
+import { ClinicCard } from "@/features/clinic/components/ui/card";
+import { Form as EditPatientForm } from "@/lezzform/_generated/addpatientwithoutvitalform";
+import { useGetPatient } from "@/services/patient/hooks/use-get-patient";
+import { useUpdatePatient } from "@/services/patient/hooks/use-update-patient";
+import type { UpdatePatientDto } from "@/services/patient/types/dto";
+import { PatientQueryKeyFactory } from "@/services/patient/utils/query-key.factory";
 
-// import { ClinicPatientTable } from "../components/table";
-
-export function ClinicEditPatientPage(): JSX.Element {
+export function ClinicEditPatientPage(): JSX.Element | undefined {
+  const queryClient = useQueryClient();
+  const router = useRouter();
   const pathname = usePathname();
-  const { medicalRecordNumber } = useParams();
+  const { toast } = useToast();
+
+  const { clinicId, patientId } = useParams();
+
+  const { data: patientData } = useGetPatient(patientId as string);
+  const { mutateAsync } = useUpdatePatient(patientId as string);
+
+  const onSubmit = useCallback(
+    async (_form: object, dto: Record<string, unknown>) => {
+      const formattedData: UpdatePatientDto = {
+        nik: dto.nik as string,
+        fullname: dto.fullname as string,
+        address: dto.address as string,
+        sex: dto.sex as string,
+        blood: dto.blood as string,
+        phone: dto.phone as string,
+        birthAt: dayjs(dto.birthAt as string).format("YYYY-MM-DD"),
+      };
+
+      await mutateAsync(formattedData);
+      await queryClient.invalidateQueries({
+        queryKey: new PatientQueryKeyFactory().lists(),
+      });
+      toast({ title: "Berhasil Memperbarui Pasien!", variant: "success" });
+      router.push(`/clinic/${clinicId as string}/patient`);
+    },
+    [clinicId, mutateAsync, queryClient, router, toast],
+  );
+
+  if (!patientData) return;
 
   return (
     <div className="flex flex-col gap-4">
@@ -22,10 +61,7 @@ export function ClinicEditPatientPage(): JSX.Element {
         <BreadcrumbList>
           <BreadcrumbItem>
             <BreadcrumbLink
-              href={pathname.replace(
-                `/${medicalRecordNumber as string}/edit`,
-                ""
-              )}
+              href={pathname.replace(`/${patientId as string}/edit`, "")}
             >
               Data Pasien
             </BreadcrumbLink>
@@ -51,6 +87,16 @@ export function ClinicEditPatientPage(): JSX.Element {
           Fill the form below to edit this patient&apos;s profile
         </p>
       </div>
+
+      <ClinicCard title="Add New Patient">
+        <EditPatientForm
+          defaultValues={{
+            ...patientData,
+            birthAt: new Date(patientData.birthAt),
+          }}
+          onSubmit={onSubmit}
+        />
+      </ClinicCard>
     </div>
   );
 }
