ff6087815844e57a7dd42fff01127d988a202dfb
Ilham Adiputra <ilhamadiputra4@gmail.com>
2024-06-19 06:24:21 +0700

    fix(apps/web): build error

--- apps/web/app/auth/reset-password/page.tsx
+++ apps/web/app/auth/reset-password/page.tsx
@@ -1,5 +1,13 @@
+"use client";
+
+import { Suspense } from "react";
+
 import { ResetPasswordPage } from "@/features/reset-password/routes/reset-password-page";
 
 export default function Page(): JSX.Element {
-  return <ResetPasswordPage />;
+  return (
+    <Suspense>
+      <ResetPasswordPage />
+    </Suspense>
+  );
 }
