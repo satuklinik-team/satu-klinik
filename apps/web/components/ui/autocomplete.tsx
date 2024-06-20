3831b762e9ff9fd9de93013964454ce0f4d74666
Ilham Adiputra <ilhamadiputra4@gmail.com>
2024-06-20 13:25:08 +0700

    chore(clinic-diagnose-patient): medicine search autocomplete

---
+++ apps/web/components/ui/autocomplete.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import React, { useRef } from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
+import * as PopoverPrimitive from "@radix-ui/react-popover";
+import { Search, X } from "lucide-react";
+import { Input } from "./input";
+import { cn } from "@/lib/utils";
+import { Command, CommandEmpty, CommandGroup, CommandItem } from "./command";
+import { Button } from "./button";
+
+const Autocomplete = React.forwardRef<
+  React.ElementRef<typeof PopoverPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Root>
+>(({ children, ...props }) => {
+  return (
+    <Popover {...props}>
+      <Command className="h-fit" shouldFilter={false}>
+        {children}
+      </Command>
+    </Popover>
+  );
+});
+Autocomplete.displayName = "Autocomplete";
+
+const AutocompleteInput = React.forwardRef<
+  React.ElementRef<typeof Input>,
+  React.ComponentPropsWithoutRef<typeof Input> & {
+    display?: string;
+    onReset?: () => unknown;
+  }
+>(({ className, display, onReset, ...props }, ref) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  return (
+    <div
+      className="flex items-center border border-input rounded-md px-3 bg-transparent"
+      cmdk-input-wrapper=""
+    >
+      <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+
+      <Input
+        ref={(node) => {
+          inputRef.current = node;
+          if (typeof ref === "function") {
+            ref(node);
+          } else if (ref) {
+            ref.current = node;
+          }
+        }}
+        className={cn(
+          "outline-none h-10 outline-transparent ring-transparent border-none px-1",
+          display && "hidden",
+          className,
+        )}
+        {...props}
+      />
+
+      <div
+        className={cn(
+          "h-10 px-1 w-full flex items-center justify-between",
+          !display && "hidden",
+        )}
+      >
+        <p className="text-sm">{display}</p>
+        <Button
+          className="p-0 h-8 w-8 -mr-3"
+          size="icon"
+          variant="ghost"
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            setTimeout(() => {
+              inputRef.current?.focus();
+            }, 100);
+            onReset?.();
+          }}
+        >
+          <X size={16} />
+        </Button>
+      </div>
+    </div>
+  );
+});
+AutocompleteInput.displayName = "AutocompleteInput";
+
+const AutocompleteTrigger = PopoverTrigger;
+AutocompleteTrigger.displayName = "AutocompleteTrigger";
+
+const AutocompleteContent = React.forwardRef<
+  React.ElementRef<typeof PopoverContent>,
+  React.ComponentPropsWithoutRef<typeof PopoverContent>
+>(({ className, ...props }, ref) => {
+  return (
+    <PopoverContent
+      ref={ref}
+      className={cn(
+        "max-h-[300px] overflow-y-auto p-0 DropdownPopoverContent",
+        className,
+      )}
+      onCloseAutoFocus={(e) => {
+        e.preventDefault();
+      }}
+      onOpenAutoFocus={(e) => {
+        e.preventDefault();
+      }}
+      {...props}
+    />
+  );
+});
+AutocompleteContent.displayName = "AutocompleteContent";
+
+const AutocompleteEmpty = CommandEmpty;
+AutocompleteEmpty.displayName = "AutocompleteEmpty";
+
+const AutocompleteGroup = CommandGroup;
+AutocompleteGroup.displayName = "AutocompleteGroup";
+
+const AutocompleteItem = CommandItem;
+AutocompleteItem.displayName = "AutocompleteItem";
+
+export {
+  Autocomplete,
+  AutocompleteInput,
+  AutocompleteTrigger,
+  AutocompleteContent,
+  AutocompleteGroup,
+  AutocompleteItem,
+  AutocompleteEmpty,
+};
--- apps/web/features/clinic-diagnose-patient/components/inputs/medicine-input.tsx
+++ apps/web/features/clinic-diagnose-patient/components/inputs/medicine-input.tsx
@@ -1,22 +1,18 @@
 "use client";
 
 import { useDebounce } from "@uidotdev/usehooks";
-import { Check, ChevronsUpDown } from "lucide-react";
-import { useMemo, useState } from "react";
+import { Check } from "lucide-react";
+import { useEffect, useMemo, useState } from "react";
 
-import { Button } from "@/components/ui/button";
 import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-} from "@/components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+  Autocomplete,
+  AutocompleteContent,
+  AutocompleteEmpty,
+  AutocompleteGroup,
+  AutocompleteInput,
+  AutocompleteItem,
+  AutocompleteTrigger,
+} from "@/components/ui/autocomplete";
 import { useDisclose } from "@/hooks/use-disclose";
 import { cn } from "@/lib/utils";
 import { useFindMedicine } from "@/services/medicine/hooks/use-find-medicine";
@@ -35,7 +31,7 @@ export function MedicineInput({
   const { isOpen, setIsOpen } = useDisclose();
 
   const [search, setSearch] = useState<string>("");
-  const debouncedSearch = useDebounce(search, 300);
+  const debouncedSearch = useDebounce(search, 500);
 
   const { data } = useFindMedicine({
     search: debouncedSearch,
@@ -50,59 +46,108 @@ export function MedicineInput({
   }, [data?.data, value]);
 
   return (
-    <Popover onOpenChange={setIsOpen} open={isOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          aria-expanded={isOpen}
-          className={cn(
-            "w-full justify-between",
-            readOnly && "pointer-events-none",
-          )}
-          role="combobox"
-          variant="outline"
-        >
-          {label?.title ?? "Select medicine..."}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-96 sm:w-96 md:w-96 lg:w-96 xl:w-96 2xl:w-96 max-h-[300px] overflow-y-auto p-0">
-        <Command shouldFilter={false}>
-          <CommandInput
-            onValueChange={(commandValue) => {
-              setSearch(commandValue);
-            }}
-            placeholder="Search medicines..."
-            value={search}
-          />
-          <CommandEmpty>No medicines found.</CommandEmpty>
-          <CommandGroup>
-            {data?.data.map((item) => (
-              <CommandItem
-                key={item.id}
-                onSelect={(currentValue) => {
-                  const selectedValue = data.data.find(
-                    (option) => String(option.id) === currentValue,
-                  );
+    <Autocomplete onOpenChange={setIsOpen} open={isOpen}>
+      <AutocompleteTrigger>
+        <AutocompleteInput
+          display={label?.title}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          onReset={() => {
+            setSearch("");
+            onChange("");
+            setIsOpen(true);
+          }}
+          readOnly={readOnly}
+          value={search}
+        />
+      </AutocompleteTrigger>
+      <AutocompleteContent>
+        <AutocompleteEmpty>No medicines found.</AutocompleteEmpty>
+        <AutocompleteGroup>
+          {data?.data.map((item) => (
+            <AutocompleteItem
+              key={item.id}
+              onSelect={(currentValue) => {
+                const selectedValue = data.data.find(
+                  (option) => String(option.id) === currentValue,
+                );
 
-                  onChange(selectedValue?.id);
-                  setIsOpen(false);
-                }}
-                value={String(item.id)}
-              >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    String(value) === String(item.id)
-                      ? "opacity-100"
-                      : "opacity-0",
-                  )}
-                />
-                {item.title}
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+                setSearch(selectedValue?.title ?? "");
+                onChange(selectedValue?.id);
+                setIsOpen(false);
+              }}
+              value={String(item.id)}
+            >
+              <Check
+                className={cn(
+                  "mr-2 h-4 w-4",
+                  String(value) === String(item.id)
+                    ? "opacity-100"
+                    : "opacity-0",
+                )}
+              />
+              {item.title}
+            </AutocompleteItem>
+          ))}
+        </AutocompleteGroup>
+      </AutocompleteContent>
+    </Autocomplete>
   );
+
+  // return (
+  //   <Popover onOpenChange={setIsOpen} open={isOpen}>
+  //     <PopoverTrigger asChild>
+  //       <Button
+  //         aria-expanded={isOpen}
+  //         className={cn(
+  //           "w-full justify-between",
+  //           readOnly && "pointer-events-none",
+  //         )}
+  //         role="combobox"
+  //         variant="outline"
+  //       >
+  //         {label?.title ?? "Select medicine..."}
+  //         <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+  //       </Button>
+  //     </PopoverTrigger>
+  //     <PopoverContent className="w-96 sm:w-96 md:w-96 lg:w-96 xl:w-96 2xl:w-96 max-h-[300px] overflow-y-auto p-0">
+  //       <Command shouldFilter={false}>
+  //         <CommandInput
+  //           onValueChange={(commandValue) => {
+  //             setSearch(commandValue);
+  //           }}
+  //           placeholder="Search medicines..."
+  //           value={search}
+  //         />
+  //         <CommandEmpty>No medicines found.</CommandEmpty>
+  //         <CommandGroup>
+  //           {data?.data.map((item) => (
+  //             <CommandItem
+  //               key={item.id}
+  //               onSelect={(currentValue) => {
+  //                 const selectedValue = data.data.find(
+  //                   (option) => String(option.id) === currentValue,
+  //                 );
+
+  //                 onChange(selectedValue?.id);
+  //                 setIsOpen(false);
+  //               }}
+  //               value={String(item.id)}
+  //             >
+  //               <Check
+  //                 className={cn(
+  //                   "mr-2 h-4 w-4",
+  //                   String(value) === String(item.id)
+  //                     ? "opacity-100"
+  //                     : "opacity-0",
+  //                 )}
+  //               />
+  //               {item.title}
+  //             </CommandItem>
+  //           ))}
+  //         </CommandGroup>
+  //       </Command>
+  //     </PopoverContent>
+  //   </Popover>
 }
