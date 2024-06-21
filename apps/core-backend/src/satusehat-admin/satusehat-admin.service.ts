38dc632936938213844c0d2f481f0c67581e38bc
Muflih Naufal Maxi <muflihmaxi@gmail.com>
2024-06-21 22:27:28 +0700

    feat(apps/core-backend): create endpoint to update clinic creds

--- apps/core-backend/src/satusehat-admin/satusehat-admin.service.ts
+++ apps/core-backend/src/satusehat-admin/satusehat-admin.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateClinicCredsDto } from './dto/update-clinic-creds.dto';
+import { exclude } from 'src/utils';
 
 @Injectable()
-export class SatusehatAdminService {}
+export class SatusehatAdminService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async updateClinicCreds(dto: UpdateClinicCredsDto) {
+    return await this.prismaService.clinics.update({
+      where: {
+        id: dto.clinicsId,
+      },
+      data: exclude(dto, ['clinicsId']),
+    });
+  }
+}
