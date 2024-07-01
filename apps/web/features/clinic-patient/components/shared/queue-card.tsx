0913df77475d9918d506754e18252786fd8bf040
Ilham Adiputra <ilhamadiputra4@gmail.com>
2024-07-01 08:50:30 +0700

    chore(clinic-patient): queue add ping animation

--- apps/web/features/clinic-patient/components/shared/queue-card.tsx
+++ apps/web/features/clinic-patient/components/shared/queue-card.tsx
@@ -1,6 +1,5 @@
 import type { HTMLAttributes } from "react";
 
-import { Badge } from "@/components/ui/badge";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import type { PatientEntity } from "@/services/patient/types/entity";
@@ -25,7 +24,7 @@ export function QueueCard({
     <Card
       className={cn(
         "flex-row justify-between items-center gap-2 px-4 py-3",
-        className,
+        className
       )}
       {...rest}
     >
@@ -47,7 +46,10 @@ export function QueueCard({
         </p>
       </div>
       {Boolean(isActive) && (
-        <Badge className="p-0 w-3 h-3 rounded-full" variant="destructive" />
+        <span className="relative flex h-3 w-3">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-destructive opacity-75" />
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-destructive" />
+        </span>
       )}
     </Card>
   );
